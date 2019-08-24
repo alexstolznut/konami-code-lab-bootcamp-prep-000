d9313908871c523ccfa03c040cfb4061b9b6623b
Alex Stolzoff <alexstolznut@hotmail.com>
2019-08-24 01:03:56 +0000

    Automatically backed up by Learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,7 +1,7 @@
 // const expect = chai.expect;
-// 
+//
 // describe("index.js", () => {
-// 
+//
 //   const codes = [
 //     "ArrowUp",
 //     "ArrowUp",
@@ -14,20 +14,20 @@
 //     "b",
 //     "a"
 //   ];
-// 
+//
 //   function triggerKeyDown(key) {
 //     const keyboardEvent = new KeyboardEvent("keydown", { key });
 //     document.body.dispatchEvent(keyboardEvent);
 //   }
-// 
+//
 //   init()
-// 
+//
 //   describe("Konami code", () => {
-// 
+//
 //     const spy = sinon.stub(window, "alert");
-// 
+//
 //     it("triggers an alert if the right code is entered", () => {
-// 
+//
 //       for (let i = 0, l = codes.length+1; i < l; i++) {
 //         console.log(codes.length);
 //         triggerKeyDown(codes[i]);
@@ -35,15 +35,15 @@
 //       expect(spy.called).to.equal(true)
 //       expect(spy.callCount).to.equal(1)
 //     });
-// 
+//
 //     it("does not trigger an alert if the wrong code is entered", () => {
 //       spy.reset()
-// 
+//
 //       for (let i = 0; i < codes.length - 1; i++) {
 //         triggerKeyDown(codes[i])
 //       }
 //       triggerKeyDown("ArrowUp")
-// 
+//
 //       expect(spy.notCalled).to.equal(true);
 //     });
 //   });
