dd79501e7642a75cb0fe5e2742ffbb32da17148d
Alex Stolzoff <alexstolznut@hotmail.com>
2019-08-24 01:02:55 +0000

    Automatically backed up by Learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -29,7 +29,7 @@ describe("index.js", () => {
     it("triggers an alert if the right code is entered", () => {
 
       for (let i = 0, l = codes.length; i < l; i++) {
-        it(codes.length,'codes length');
+        console.log(codes.length)
         triggerKeyDown(codes[i]);
       }
       expect(spy.called).to.equal(true)
