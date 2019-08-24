e47b2bdf4c92a98e95411014b92fba145d56ccb1
Alex Stolzoff <alexstolznut@hotmail.com>
2019-08-24 00:40:36 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -27,6 +27,7 @@ function init() {
       if(codeStore.length === codes.length+1 ) {
           if(JSON.stringify(codeStore) === JSON.stringify(codes)){
             alert('Congratulations, you cracked the code!');
+            console.log('Congratulations, you cracked the code!');
             codeStore = [];
             index = 0;
             return;
